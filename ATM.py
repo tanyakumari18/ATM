474d19acaf0679f5fed2018a3d263c0950b943d6
Your Name <you@example.com>
2022-06-03 14:06:17 +0530

    atm

---
+++ ATM.py
@@ -0,0 +1,17 @@
+# create Accounts.txt if not exist
+try:
+    # read the 'Accounts.txt' file
+    # if you try to open non existing file in read mode, this will throw an error
+    f = open('Accounts.txt', 'r')
+    f.close()
+except FileNotFoundError:
+    # if 'Accounts.txt' file is not found, create it
+    f = open('Accounts.txt', 'w')
+    f.close()
+
+# import modules
+from menu1 import menu1
+import os
+
+os.system('clear')
+menu1()  # start the program - call menu1() function
